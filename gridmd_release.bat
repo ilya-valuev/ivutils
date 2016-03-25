1cd49fc189acbc342397a9d2f5ad20e3a0d5781b
Ilya Valuev <valuev@physik.hu-berlin.de>
2016-03-25 11:53:22 +0300

    added  bat file (template) to produce gridmd release

---
+++ gridmd_release.bat
@@ -0,0 +1,14 @@
+rem list of files needed in GridMD project
+
+CMakeLists.txt
+doxygen\Doxyfile
+doxygen\readme.txt
+include\logexc.h
+include\plane_3.h
+include\refobj.h
+include\seqpack.h
+include\utiltl.h
+include\vector_3.h
+sources.cmake
+src\logexc.cpp
+src\seqpack.cpp
